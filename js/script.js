c608cc9ac4731d7ef8ff9a920b91026cabba8fbf
CristianoIsaias <crisano79@gmail.com>
2026-01-04 08:01:13 -0300

    atribuindo interatividade javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,55 @@
+const nome = document.querySelector('#nome')
+const email = document.querySelector('#email')
+const telefone = document.querySelector('#telefone')
+const assunto = document.querySelector('#assunto')
+const formulario = document.querySelector('#formulario')    
 
+// Efeito de escrita de máquina de escrever no primeiro anúncio do site.
+const anuncio1 = document.getElementById('anuncio1').textContent;
+let i=0;
+let texto = "";
+// Primeiro anuncio do site
+function mostrarAnuncio(){
+        if(i < anuncio1.length){
+            texto += anuncio1.charAt(i);
+            document.getElementById('anuncio1').textContent = texto;    
+            i++;
 
+            setTimeout(mostrarAnuncio, 100);}
+
+}
+mostrarAnuncio();
+
+// Validando o formulário
+formulario.addEventListener('submit', (e) => {
+    if(!nome.value || !email.value || !telefone.value || !assunto.value){
+
+nome.addEventListener('input', () => {
+    nome.value = nome.value.replace(/[^a-zA-Z\s]/g, '')
+})
+email.addEventListener('input', () => {
+    email.value = email.value.replace(/[^a-zA-Z0-9@._-]/g, '')
+})
+
+telefone.addEventListener('input', () => {
+    telefone.value = telefone.value.replace(/[^0-9]/g, '')
+}) 
+
+assunto.addEventListener('input', () => {
+    assunto.value = assunto.value.replace(/[^a-zA-Z0-9\s.,!?]/g, '')
+})
+
+    }else{
+        alert('Formulário enviado com sucesso!')
+    }
+    e.preventDefault()
+    alert('Por favor preencha todos campos do Formulário!')
+
+    
+    formulario.reset()
+})  
+
+// Efeito para carrossel de scroll
 const myObserver = new IntersectionObserver((entries)=>{
    entries.forEach( (entry) => {
     if(entry.isIntersecting){
@@ -14,4 +64,13 @@ const efeito = document.querySelectorAll('.hidden')
 
 efeito.forEach((element) => myObserver.observe(element))
     
+//   Efeito do botão enviar no formulário
+function btnColor(){
+    const btn = document.querySelector('#btn-enviar')
+    btn.style.backgroundColor =  '#0177FB' 
+}
 
+function btnColorOut(){
+    const btn = document.querySelector('#btn-enviar')
+    btn.style.backgroundColor = '#265180'
+}
